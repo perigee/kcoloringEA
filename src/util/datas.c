4d068ac14d4c7867508d5d1d032dc0affd2c7e06
perigee <junhubbs@gmail.com>
2011-11-27 00:57:35 +0100

    binary tree

--- src/util/datas.c
+++ src/util/datas.c
@@ -2,6 +2,88 @@
 #include "datas.h"
 
 
-struct nodeBTree{
+///========================================
+/// node structure for Binary Tree
+///========================================
 
+
+typedef int* ElementType;
+typedef int KeyType;
+
+typedef struct TreeNode{
+  KeyType key;  // the comparing key of node
+  ElementType* element; 
+  struct TreeNode* left;
+  struct TreeNode* right;
+} TreeNode;
+
+
+/*!
+ * Create tree node
+ */
+TreeNode* createNode(KeyType key, ElementType e){
+  TreeNode* tempNode;
+  tempNode = malloc(sizeof(TreeNode));
+  tempNode->key = key;
+  tempNode->element = e;
+  tempNode->left = NULL;
+  tempNode->right = NULL;
+  return tempNode;
 }
+
+
+
+/*!
+ * Insert the node in the tree by order
+ * @return the root node
+ */
+TreeNode* insertNode(TreeNode* root,TreeNode* node){
+  
+  if(root == NULL)
+    return node;
+  
+  if (root->key => node->key) 
+    node->left = insertNode(root->left, node); 
+  else 
+    node->right = insertNode(root->right, node);
+
+  return root;
+}
+
+TreeNode* insertNode(TreeNode* root, KeyType key, ElementType e){
+  TreeNode* tmpNode = malloc(sizeof(TreeNode));
+  tmpNode->key = key;
+  tmpNode->element = e;
+  return insertNode(root, tmpNode);
+}
+
+
+
+/*!
+ * Traveral the tree
+ */
+
+
+
+
+/*!
+ * Delete entire tree
+ */
+void deleteTree(TreeNode* root){
+  if(root->left != NULL){
+    deleteTree(root->left);
+  }
+
+  if(root->right != NULL){
+    deleteTree(root->right);
+  }
+  
+  free(root);
+  root = NULL;
+}
+
+
+
+
+
+
--- src/util/datas.h
+++ src/util/datas.h
@@ -5,5 +5,10 @@
 
 
 
+struct TreeNode;
+TreeNode* createNode(KeyType key, ElementType e);
+TreeNode* insertNode(TreeNode* root,TreeNode* node);
+TreeNode* insertNode(TreeNode* root, KeyType key, ElementType e);
+void deleteTree(TreeNode* root);
 
 #endif
