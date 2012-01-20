ec288455add8f2be3865f8d6084e41e2f5090330
perigee <junhubbs@gmail.com>
2012-01-20 15:59:47 +0100

    merge with jhu's dev/heuristic branch

--- src/main.c
+++ src/main.c
@@ -12,23 +12,18 @@ int main (int argc, char * const argv[]) {
 
 
   //================================== IIS inspired algorithm ================ BGN
-  /*
-  for (int i=0; i<100; ++i){
-    double tval=(rand()/(double)RAND_MAX);
-    printf("%f\n",tval);
-  }*/
 
-  
+  /*
   srand((unsigned)time(NULL));
   printf("d: Procedure de calcul \n");
-  /*
+  
   if(argc<5){
     printf("Erreur d'utilisation !!\n");
     exit(-1);
-    }*/
+    }
   testAlgo(argv[1], argv[2],argv[3],argv[4],argv[5],
 	   argv[6], argv[7]);
-  return 0;
+  return 0;*/
   //================================== IIS inspired algorithm ================ END
 
 
